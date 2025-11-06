83e6004b7f4149e7c429ea2ae5e6d97c6a647038
Aryan <aryanyadav182007@gmail.com>
2025-11-06 08:33:03 +0000

    Event Listener

---
+++ Java-Script/CLASS-WORK/DOM/Lecture-3/app.js
@@ -0,0 +1,11 @@
+//Event Listener=============================================================
+
+const body = document.querySelector("body");
+const box = document.createElement("div");
+const btn = document.createElement("button");
+body.appendChild(box);
+body.appendChild(btn);
+btn.textContent = "Hit meee";
+box.style.height = "200px";
+box.style.width = "200px";
+box.style.backgroundColor = "beige";
