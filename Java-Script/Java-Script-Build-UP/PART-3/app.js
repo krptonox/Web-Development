59e722cd5ee72f0d26375cb215351b89eac89306
Aryan <aryanyadav182007@gmail.com>
2026-02-09 23:56:57 +0000

    Stack and heap memory

--- Java-Script/Java-Script-Build-UP/PART-3/app.js
+++ Java-Script/Java-Script-Build-UP/PART-3/app.js
@@ -17,3 +17,20 @@ console.log(Boolean(undefined));// falsy
 
 console.log(5 == "5"); 
 console.log(5 === "5"); 
+
+//Memory 
+
+//Stack (primitive data types) and Heap (reference data types)
+
+//In stack , value give copy of the value to another variable
+let a = 10;
+let b = a;
+console.log(a); // 10
+
+let arr1 = [1, 2, 3];
+let arr2 = arr1;
+console.log(arr1); // [1, 2, 3]
+
+arr1.push(4);
+console.log(arr1);
+console.log(arr2); // [1, 2, 3, 4] because arr1 and arr2 reference the same array in heap
