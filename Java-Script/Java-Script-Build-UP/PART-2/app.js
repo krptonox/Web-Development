1041ea11f334fa1c8d3f1111b99ce13d2a15cc37
Aryan <aryanyadav182007@gmail.com>
2026-02-09 22:02:38 +0000

    data type

---
+++ Java-Script/Java-Script-Build-UP/PART-2/app.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3 + 4) It is using node.js , not browser
+
+
+let name = "knox";
+let age = 18;
+let islog = false;
+
+console.log(name);
+console.log(age);
+console.log(islog);
+
+console.log(typeof name);
+console.log(typeof null); // null is an object in js
