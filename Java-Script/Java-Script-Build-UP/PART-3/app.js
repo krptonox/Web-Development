02fa10e78e79bfbd8e884e465f01c78dc314fee1
Aryan <aryanyadav182007@gmail.com>
2026-02-09 22:22:05 +0000

    Data type and conversion

---
+++ Java-Script/Java-Script-Build-UP/PART-3/app.js
@@ -0,0 +1,19 @@
+console.log(null + 1);
+console.log("5" + 3);
+console.log("5" - 3);
+console.log(true + false);
+
+
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof 123);
+
+
+console.log(Boolean(0)); // falsy
+console.log(Boolean("0")); // truthy
+console.log(Boolean([])); // truthy
+console.log(Boolean(undefined));// falsy
+
+
+console.log(5 == "5"); 
+console.log(5 === "5"); 
