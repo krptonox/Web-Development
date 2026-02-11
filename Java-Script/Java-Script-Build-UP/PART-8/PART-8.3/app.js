3f244119f9ae125dd04a01dcea239a994af1cf15
Aryan <aryanyadav182007@gmail.com>
2026-02-11 19:36:06 +0000

    Object and json data

---
+++ Java-Script/Java-Script-Build-UP/PART-8/PART-8.3/app.js
@@ -0,0 +1,50 @@
+//deStructuring of objects
+
+const person = {
+    name: 'John',
+    age: 30,
+    city: 'New York'
+};
+
+// const name = person.name;
+// const age = person.age;
+console.log(person.name);
+
+
+const githubApi = "https://api.github.com/users/krptonox";
+
+const jsonData = {
+  "login": "krptonox",
+  "id": 188233023,
+  "node_id": "U_kgDOCzg1Pw",
+  "avatar_url": "https://avatars.githubusercontent.com/u/188233023?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/krptonox",
+  "html_url": "https://github.com/krptonox",
+  "followers_url": "https://api.github.com/users/krptonox/followers",
+  "following_url": "https://api.github.com/users/krptonox/following{/other_user}",
+  "gists_url": "https://api.github.com/users/krptonox/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/krptonox/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/krptonox/subscriptions",
+  "organizations_url": "https://api.github.com/users/krptonox/orgs",
+  "repos_url": "https://api.github.com/users/krptonox/repos",
+  "events_url": "https://api.github.com/users/krptonox/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/krptonox/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Aryan",
+  "company": null,
+  "blog": "https://glistening-froyo-64dc4d.netlify.app/",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": "Hey, I'm Aryan and I am 💻 Computer Science Student | 🚀 Learning DSA, Java & Web Development \r\n📚 Sharing my LeetCode solutions and  Web-D journey.........",
+  "twitter_username": null,
+  "public_repos": 6,
+  "public_gists": 0,
+  "followers": 9,
+  "following": 11,
+  "created_at": "2024-11-12T17:15:16Z",
+  "updated_at": "2026-02-09T12:15:03Z"
+}
